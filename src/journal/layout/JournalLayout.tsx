05dd26999113c1ebae56fb27657d6499abf82a54
JeanS <jean.sinche@alarcongroup.us>
2024-10-06 09:08:17 -0500

    feat(journal): Set up basic journal layout
    Creates a new layout component (`JournalLayout`) for the journal feature and uses it to structure the `JournalPage`.  The layout currently includes placeholders for a navbar, sidebar, toolbar, as well as potential content like a note view or a "nothing selected" message.

---
+++ src/journal/layout/JournalLayout.tsx
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material"
+
+export interface JournalLayoutProps {
+    children: React.ReactNode
+}
+const drawerWidth = 240;
+export const JournalLayout = ({ children }: JournalLayoutProps) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            {/* Navbar drawerWidth*/}
+            {/* Sidebar drawerWidth*/}
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, p: 3 }}
+            >
+                {/* Toobar */}
+                {children}
+            </Box>
+        </Box>
+    )
+}
--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,9 +1,12 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">JournalPage</Typography>
-        </>
+        <JournalLayout>
+            <Typography >Eiusmod eu ex est proident id et laboris sint amet reprehenderit culpa non elit ea. Anim in qui excepteur irure ad quis ipsum. Culpa velit voluptate voluptate exercitation enim cupidatat et. Voluptate est nostrud dolore quis sit labore do qui non. Reprehenderit magna aliquip pariatur aliquip tempor sit et cillum sit esse mollit ex adipisicing irure. Cillum qui aute et proident aute exercitation velit proident sint. Laboris deserunt ex deserunt consequat qui.</Typography>
+            {/* NothingSelected */}
+            {/* NoteView */}
+        </JournalLayout>
     )
 }
