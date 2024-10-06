a4e7922e291c6211ea4b56ca39c5e9e923877d96
JeanS <jean.sinche@alarcongroup.us>
2024-10-05 21:00:37 -0500

    feat(auth): Implement basic login form UI
    Creates a visually appealing and functional login form, setting the groundwork for user authentication. This includes input fields for email and password, along with basic styling for an improved user experience.

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,6 +1,43 @@
+import { Grid2, TextField, Typography } from '@mui/material';
 
 export const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <Grid2
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+        >
+            <Grid2
+                className="box-shadow"
+                size={{ xs: 3 }}
+                sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+            >
+                <Typography variant="h5" sx={{ mb: 2 }}>Login</Typography>
+                <form action="">
+                    <Grid2 container
+                        spacing={2}>
+                        <Grid2 size={{ xs: 12 }} >
+                            <TextField
+                                label="Correo"
+                                type="email"
+                                placeholder='correo@google.com'
+                                fullWidth
+                            />
+                        </Grid2>
+                        <Grid2 size={{ xs: 12 }}>
+                            <TextField
+                                label="Contraseña"
+                                type="password"
+                                placeholder='Contraseña'
+                                fullWidth
+                            />
+                        </Grid2>
+                    </Grid2>
+                </form>
+            </Grid2>
+        </Grid2>
     )
 }
