c60de517727a0b2d50518f6be82d6da70b34c439
JeanS <jean.sinche@alarcongroup.us>
2024-10-05 22:44:39 -0500

    feat(UI): Implement LoginPage UI
    Implements a styled login form with email and password fields, login and Google authentication buttons, and a link to registration. The layout is responsive and visually enhanced for a better user experience.

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,8 +1,10 @@
-import { Grid2, TextField, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid2 as Grid, Link, TextField, Typography } from '@mui/material';
 
 export const LoginPage = () => {
     return (
-        <Grid2
+        <Grid
             container
             spacing={0}
             direction="column"
@@ -10,34 +12,69 @@ export const LoginPage = () => {
             justifyContent="center"
             sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
         >
-            <Grid2
+            <Grid
                 className="box-shadow"
-                size={{ xs: 3 }}
+                size={{ xs: 12, md: 6, lg: 4 }}
                 sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
             >
                 <Typography variant="h5" sx={{ mb: 2 }}>Login</Typography>
                 <form action="">
-                    <Grid2 container
+                    <Grid container
                         spacing={2}>
-                        <Grid2 size={{ xs: 12 }} >
+                        <Grid size={{ xs: 12 }} >
                             <TextField
                                 label="Correo"
                                 type="email"
                                 placeholder='correo@google.com'
                                 fullWidth
                             />
-                        </Grid2>
-                        <Grid2 size={{ xs: 12 }}>
+                        </Grid>
+                        <Grid size={{ xs: 12 }}>
                             <TextField
                                 label="Contraseña"
                                 type="password"
                                 placeholder='Contraseña'
                                 fullWidth
                             />
-                        </Grid2>
-                    </Grid2>
+                        </Grid>
+
+                        <Grid
+                            container
+                            spacing={2}
+                            sx={{ mb: 2, mt: 2 }}
+                            size={{ xs: 12 }}
+                        >
+                            <Grid size={{ xs: 12, md: 6 }}>
+                                <Button variant='contained' fullWidth>
+                                    Login
+                                </Button>
+                            </Grid>
+                            <Grid size={{ xs: 12, md: 6 }}>
+                                <Button variant='contained' fullWidth>
+                                    <Google />
+                                    <Typography sx={{ ml: 1 }}>
+                                        Google
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+                        <Grid
+                            container
+                            direction='row'
+                            justifyContent='end'
+                            size={{ xs: 12 }}
+                        >
+                            <Link
+                                component={RouterLink}
+                                color='inherit'
+                                to='/auth/register'>
+                                Crear una cuenta
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </form>
-            </Grid2>
-        </Grid2>
+            </Grid>
+        </Grid>
     )
 }
